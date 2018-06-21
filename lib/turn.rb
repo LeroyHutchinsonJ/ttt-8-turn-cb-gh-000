79d3ef582b6ffa5f0bef6fdd9c51c2850437839f
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-21 01:56:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ return index
 end
 
 def valid_move?(board,index)
-if(index >= 0 && index <= 8 && position_taken?(board,index) == false)
+if(index.between?(0,8) && position_taken?(board,index) == false)
 true
 end
 end
