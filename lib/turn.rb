bdbe621b30b86ee028d8cd4a667d4207d507dbfa
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-21 02:08:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,16 +12,18 @@ index = input.to_i - 1
 return index
 end
 
-def valid_move?(board,index)
-if(index.between?(0,8) && position_taken?(board,index) == false)
-true
-end
-end
+
 
 def position_taken?(board, index)
 if(board[index] == "" || board[index] == " " || board[index] == nil)
 false
 elsif board[index] == "X" || board[index] == "O"
+true
+end
+end
+
+def valid_move?(board,index)
+if(index.between?(0,8) && position_taken?(board,index) == false)
 true
 end
 end
