37a780ad0274b02a920fd36f69d8ad5d592d29d9
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-21 01:02:32 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,26 @@
+def display_board
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+
+def valid_move?(board,index)
+if(index >= 0 && index <= 8 && position_taken?(board,index) == false)
+true
+end
+end
+
+def position_taken?(board, index)
+if(board[index] == "" || board[index] == " " || board[index] == nil)
+false
+elsif board[index] == "X" || board[index] == "O"
+true
+end
+end
+
+def move(board,index, token = 'X')
+  board[index] = token
+end
